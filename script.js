2284b0f09d60fe7ab5a42af6e5dea182da4da4bb
Chester2059 <chester_2059@hotmail.com>
2024-05-04 09:48:51 -0300

    git

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const img = document.querySelector("#profile img")
 
 if (html.classList.contains("light")) {
   img.setAttribute("src", "./assets/avatar-light.png")
-  img.setAttribute("alt", "Foto de um japa")
+  img.setAttribute("alt", "Foto de Gustavo Mai com fundo roxo")
 } else {
     img.setAttribute("src", "./assets/Avatar.png")
-    img.setAttribute("alt", "Foto de Gustavo Mai")}
+    img.setAttribute("alt", "Foto de Gustavo Mai com fundo amarelo")}
   }
